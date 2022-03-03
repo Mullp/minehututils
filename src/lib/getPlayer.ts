34052294bba8e0fb96dc9591b8875a05da04ba08
Mullp <mullp@protonmail.com>
2022-03-03 22:05:29 +0100

    Update getPlayer.ts

--- src/lib/getPlayer.ts
+++ src/lib/getPlayer.ts
@@ -14,38 +14,37 @@ const uuidCache = new NodeCache({
 
 export async function getPlayer(player: string) {
   if (
-    !nameCache.has(player.toLowerCase()) &&
-    !uuidCache.has(player.toLowerCase())
-  ) {
-    return await fetch(
-      `https://api.mojang.com/users/profiles/minecraft/${player}`
-    )
-      .then((data: any) => data.json())
-      .then(async (apiPlayer: ApiPlayer) => {
-        nameCache.set(apiPlayer.name.toLowerCase(), apiPlayer);
-        uuidCache.set((await idToUuid(apiPlayer.id)).toLowerCase(), apiPlayer);
-        return apiPlayer;
-      })
-      .catch(async () => {
-        return await fetch(
-          `https://sessionserver.mojang.com/session/minecraft/profile/${player}`
-        )
-          .then((data: any) => data.json())
-          .then(async (apiPlayer: ApiPlayer) => {
-            nameCache.set(apiPlayer.name.toLowerCase(), apiPlayer);
-            uuidCache.set(
-              (await idToUuid(apiPlayer.id)).toLowerCase(),
-              apiPlayer
-            );
-            return apiPlayer;
-          })
-          .catch(() => {
-            return;
-          });
-      });
-  } else {
+    nameCache.has(player.toLowerCase()) ||
+    uuidCache.has(player.toLowerCase())
+  )
     return nameCache.has(player.toLowerCase())
       ? (nameCache.get(player.toLowerCase()) as ApiPlayer)
       : (uuidCache.get(player.toLowerCase()) as ApiPlayer);
-  }
+
+  return await fetch(
+    `https://api.mojang.com/users/profiles/minecraft/${player}`
+  )
+    .then((data: any) => data.json())
+    .then(async (apiPlayer: ApiPlayer) => {
+      nameCache.set(apiPlayer.name.toLowerCase(), apiPlayer);
+      uuidCache.set((await idToUuid(apiPlayer.id)).toLowerCase(), apiPlayer);
+      return apiPlayer;
+    })
+    .catch(async () => {
+      return await fetch(
+        `https://sessionserver.mojang.com/session/minecraft/profile/${player}`
+      )
+        .then((data: any) => data.json())
+        .then(async (apiPlayer: ApiPlayer) => {
+          nameCache.set(apiPlayer.name.toLowerCase(), apiPlayer);
+          uuidCache.set(
+            (await idToUuid(apiPlayer.id)).toLowerCase(),
+            apiPlayer
+          );
+          return apiPlayer;
+        })
+        .catch(() => {
+          return;
+        });
+    });
 }

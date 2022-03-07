389a62a47802ca622dea8c84e1f1d22b78360520
Mullp <mullp@protonmail.com>
2022-03-07 13:56:57 +0100

    Update addInviteField.ts

--- src/lib/addInviteField.ts
+++ src/lib/addInviteField.ts
@@ -6,10 +6,12 @@ export async function addInviteField(embed: MessageEmbed) {
     "\u200B",
     `${hyperlink(
       `${formatEmoji("946429850825138196")} Join the official support server!`,
-      "https://discord.gg/uUejGQ8kGu"
+      "https://discord.gg/uUejGQ8kGu",
+      "Click to join!"
     )}  |  ${hyperlink(
       "Invite me to your own server!",
-      "https://discord.com/api/oauth2/authorize?client_id=874273664131010560&permissions=0&scope=bot%20applications.commands"
+      "https://discord.com/api/oauth2/authorize?client_id=874273664131010560&permissions=0&scope=bot%20applications.commands",
+      "Click to invite the bot!"
     )}`
   );
 }
